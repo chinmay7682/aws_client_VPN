06445de7183d95c51dd0c90ab3fb8fca91eec4c6
chinmay.m.fiscloud <chinmay.m.fiscloud@aux.gojek.com>
2023-01-15 17:40:14 +0530

    "Updated Readme file"

--- providers.tf
+++ providers.tf
@@ -1,4 +1,7 @@
-#Below is the required version for aws provider
+#Below is the provider configuration with Shared Configuration and Credentials Files.
 provider "aws" {
-  region = "ap-south-1"
+  region                   = "ap-south-1"
+  shared_config_files      = ["/Users/vanch/.aws/config"]
+  shared_credentials_files = ["/Users/vanch/.aws/credentials"]
+  profile                  = "AWSAmbassador"
 }
