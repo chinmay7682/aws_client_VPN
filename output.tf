5e7d8ad80cd26edad37da1b37acce9491f28ac9a
chinmay7682 <65828245+chinmay7682@users.noreply.github.com>
2023-01-16 11:48:21 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,6 +1,8 @@
 output "clientvpn-id" {
-  value = aws_ec2_client_vpn_endpoint.clientvpn.id
+  description = "The ID of the Client VPN endpoint."
+  value       = aws_ec2_client_vpn_endpoint.clientvpn.id
 }
 output "dns-name" {
-  value = aws_ec2_client_vpn_endpoint.clientvpn.dns_name
+  description = "The DNS name to be used by clients when establishing their VPN session."
+  value       = aws_ec2_client_vpn_endpoint.clientvpn.dns_name
 }
